28d2bedd4580b97282b78221833624e97bd8ea9d
tonganyuan <tonganyuan@ynby.cn>
2022-12-03 08:46:08 +0800

    教学demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,131 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+      <el-container>
+        <el-header height="80px" style="padding:0">
+          <div class="header">教务系统学生管理</div>
+        </el-header>
+        <el-container>
+          <el-aside width="200px">
+            <el-menu class="aside" @select="selectFunc" default-active="1" :unique-opened="true">
+              <el-submenu index="1">
+                <template #title>
+                  <span>七年级</span>
+                </template>
+                <el-menu-item index="1">1班</el-menu-item>
+                <el-menu-item index="2">2班</el-menu-item>
+                <el-menu-item index="3">3班</el-menu-item>
+              </el-submenu>
+              <el-submenu index="2">
+                <template #title>
+                  <span>八年级</span>
+                </template>
+                <el-menu-item index="4">1班</el-menu-item>
+                <el-menu-item index="5">2班</el-menu-item>
+                <el-menu-item index="6">3班</el-menu-item>
+              </el-submenu>
+              <el-submenu index="3">
+                <template #title>
+                  <span>九年级</span>
+                </template>
+                <el-menu-item index="7">1班</el-menu-item>
+                <el-menu-item index="8">2班</el-menu-item>
+                <el-menu-item index="9">3班</el-menu-item>
+              </el-submenu>
+            </el-menu>
+          </el-aside>
+          <el-container>
+            <el-header height="80px" style="padding: 0;margin: 0">
+              <el-container class="subHeader">
+                <div class="desc">
+                  {{desc}}
+                </div>
+                <el-button style="margin: 20px;width: 100px;height: 30px;">新增记录</el-button>
+              </el-container>
+            </el-header>
+            <el-main style="margin: 0;padding:0">
+              <div class="content">
+                <el-table :data="stus">
+                  <el-table-column prop="name" lable="姓名"></el-table-column>
+                  <el-table-column prop="age" lable="年龄"></el-table-column>
+                  <el-table-column prop="sex" lable="性别"></el-table-column>
+                  <el-table-column prop="date" lable="录入日期"></el-table-column>
+                </el-table>
+              </div>
+            </el-main>
+            <el-footer height="30px" class="footer">
+              Vue 框架搭建，Element-Plus提供组件支持 
+            </el-footer>
+          </el-container>
+        </el-container>
+      </el-container>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .header {
+    font-size: 30px;
+    line-height: 80px;
+    background-color: red;
+  }
+  .aside {
+    background-color: wheat;
+    height: 600px;
+  }
+  .subHeader {
+    background-color: cornflowerblue;
+  }
+  .desc{
+    font-size: 25px;
+    line-height: 80px;
+    color: white;
+    width: 800px;
+  }
+  .content {
+    height: 410px;
+  }
+  .footer {
+    background-color: dimgray;
+    color: white;
+    font-size: 17px;
+    line-height: 30px;
+  }
+
 </style>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data(){
+      return {
+        desc:"七年级1班学生统计",
+        stus:[
+          {
+            name:"小王",
+            age:14,
+            sex:"男",
+            date:"2022年11月26号"
+          },
+          {
+            name:"小张",
+            age:15,
+            sex:"女",
+            date:"2022年11月26号"
+          },
+          {
+            name:"小秋",
+            age:15,
+            sex:"女",
+            date:"2022年11月26号"
+          }
+        ]
+      }
+    },
+    methods:{
+      selectFunc(index) {
+        let strs = ["七","八","九"]
+        let rank = strs[Math.floor((index-1)/3)]
+        this.desc = `${rank}年级${((index-1) % 3) + 1}班学生统计`
+      }
+    }
+  }
+</script>
